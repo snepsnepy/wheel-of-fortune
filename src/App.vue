37f234ac70a4a0b93086fc61dbbc5537f6cb8a6f
snepsnepy <snepysneptest@gmail.com>
2025-06-24 15:21:33 +0300

    Added wheel with prediction

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div class="app">
-    <WheelOfFortune />
+    <WheelOfFortune :spinTrigger="spinCounter" desiredResult="WIN" />
+    <button class="spin-button" @click="triggerSpin">SPIN</button>
+    {{ spinCounter }}
   </div>
 </template>
 
 <script setup>
 import WheelOfFortune from "./components/WheelOfFortune.vue";
+import { ref } from "vue";
+
+const spinCounter = ref(0);
+
+const triggerSpin = () => {
+  spinCounter.value++; // Change the prop to trigger a spin
+};
 </script>
+
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #333;
+}
+
+.spin-button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1.1rem;
+  font-weight: bold;
+  background-color: #fca311;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background 0.3s ease;
+}
+
+.spin-button:hover {
+  background-color: #e59200;
+}
+</style>
